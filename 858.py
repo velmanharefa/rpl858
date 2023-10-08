96403d463da5c72917d06cecc0fc8790a773958e
velmanharefa <velmanh24@gmail.com>
2023-10-08 19:48:41 +0700

    kondisi
    lanjutkan

--- 858.py
+++ 858.py
@@ -7,3 +7,41 @@ print("4.Delete Auto TP")
 print("5.List Auto TP")
 print("6.Cek Kupon Undian TP")
 
+angka = int(input())
+
+if angka == 1:
+    print("Silahkan masukkan nomor tujuan Transfer Pulsa:")
+    print("(contoh: 08xxxx atau 628xxxx)")
+    a1 = int(input())
+    print("Silahkan masukkan jumlah pulsa yang akan")
+    print("ditransfer : (min 5000, max 1jt & tanpa. (titik)")
+    print("atau, (koma))")
+    b1 = int(input())
+elif angka == 2:
+    print("Silahkan masukkan nomor tujuan Minta Pulsa:")
+    print("(contoh: 08xxxx atau 628xxxx)")
+    a2 = int(input())
+elif angka == 3:
+    print("Silahkan masukkan nomor tujuan yang anda Auto Transfer Pulsa:")
+    a3 = int(input())
+elif angka == 4:
+    print("Silahkan masukkan nomor tujuan yg akan")
+    print("dihapus dari list Auto Transfer Pulsa:")
+    a4 = int(input())
+elif angka == 5:
+    print("Terima kasih permintaan Anda sedang diproses.")
+    print("Sampaikan pd JIWA BERSEDIH! Lagu viral")
+    print("dr Ghea Indrawari di LangitMusik Midium")
+    print("Rp4400/3hr. Mau?")
+    print("1.Ya")
+    print("2.Tidak")
+    a5 = int(input())
+    if a5 == 1:
+        print("Terima kasih permintaan Anda sedang di proses")
+    else:
+        print("Terima kasih.")
+else:
+    print("Jumlah kupon Anda adalah : 0 Kupon")
+
+
+
