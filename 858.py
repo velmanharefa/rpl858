8b1cd5ce0e03e25ebe93e21cb717a068f07ff5ec
velmanharefa <velmanh24@gmail.com>
2023-10-08 19:46:36 +0700

    menu
    print menu

---
+++ 858.py
@@ -0,0 +1,9 @@
+print("Mau Samsung Fold 4 dr Aldi Taher?")
+print("Hub di *500*352#")
+print("1.Transfer Pulsa")
+print("2.Minta Pulsa")
+print("3.Auto TP")
+print("4.Delete Auto TP")
+print("5.List Auto TP")
+print("6.Cek Kupon Undian TP")
+
